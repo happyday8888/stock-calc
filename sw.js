7f1effc29784dd0110f93b87435d197a994ee386
happyday8888 <love558235@gmail.com>
2026-03-13 01:03:06 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,23 @@
-const CACHE_NAME = 'stock-calc-v1';
+const CACHE_NAME = 'stock-calc-v1.0.1'; // 每次更新代碼，改一下這個號碼可以強制更新快取
 const ASSETS = [
   'index.html',
   'manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // 強制跳過等待，立刻啟用新版
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
 
