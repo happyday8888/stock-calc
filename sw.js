d153c0ad4b57af9fc955978ab66604812e4121e1
happyday8888 <love558235@gmail.com>
2026-03-16 16:56:48 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// sw.js - 版本號：v20260316.21
-const CACHE_NAME = 'stock-calculator-v20260316.21';
+// sw.js - 版本號：v20260316.22
+const CACHE_NAME = 'stock-calculator-v20260316.22';
 
 // ... 安裝與激活事件保持不變 ...
 
